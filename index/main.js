7073436eb7d80aaec95a9770ffb042e910dfee20
fdlisml <fadliismail646@gmail.com>
2023-08-06 11:04:48 +0700

    p

---
+++ index/main.js
@@ -0,0 +1,98 @@
+// add hovered class to selected list item
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+}
+
+list.forEach((item) => item.addEventListener("mouser", activeLink));
+
+// Menu Toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
+
+// jam
+// To run the function on load
+document.getElementById("txt").addEventListener("load", startTime());
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+  
+    // To print AM/PM and 12 hour format  
+    if(h > 12){
+       var meridiem = "PM";
+        h = h - 12;
+       } else{
+         meridiem = "AM";
+       }
+  
+    var m = today.getMinutes();
+  
+    // add a zero in front of numbers<10
+    h = checkTime(h);
+    m = checkTime(m);
+  
+    document.getElementById("txt").innerHTML = h + ":" + m + " " + meridiem;
+    
+    // To update time every second
+    var t = setTimeout(function(){ startTime() }, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+
+// end jam
+
+// tanggal
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+const today = new Date();
+
+const weekDays = [
+  "Sun",
+  "Mon",
+  "Tues",
+  "Wed",
+  "Thurs",
+  "Fri",
+  "Sat"
+];
+
+const allMonths = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+date.innerHTML = (today.getDate() < 10 ? "0" : "") + today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
+// end tanggal
--- login/login.php
+++ login/login.php
@@ -5,76 +5,84 @@ session_start();
 
 function login($username, $password, $conn)
 {
-    $username = mysqli_real_escape_string($conn, $username);
-    $password = mysqli_real_escape_string($conn, $password);
+   $username = mysqli_real_escape_string($conn, $username);
+   $password = mysqli_real_escape_string($conn, $password);
 
-    $query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username='$username' AND password='$password'");
-    $user = mysqli_fetch_assoc($query);
+   $query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username='$username' AND password='$password'");
+   $user = mysqli_fetch_assoc($query);
 
-    if ($user) {
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
-        return true;
-    } else {
-        return false;
-    }
+   if ($user) {
+      $_SESSION['user_id'] = $user['id'];
+      $_SESSION['username'] = $user['username'];
+      return true;
+   } else {
+      return false;
+   }
 }
 
 if (isset($_POST['login'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+   $username = $_POST['username'];
+   $password = $_POST['password'];
 
-    if (login($username, $password, $conn)) {
-        header('location: index.php');
-        exit();
-    } else {
-        $error = "Username atau password salah!";
-        echo $error;
-    }
+   if (login($username, $password, $conn)) {
+      header('location:../index/index.php');
+      exit();
+   } else {
+      $error = '<script>alert("Username atau password salah!");</script>';
+      echo $error;
+   }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
-    <title>Login Page</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="login.css">
+
+   <title>Login Page</title>
 </head>
 
 <body>
 
-    <div class="container">
-        <div class="left">
-            <div class="wadah-img">
-                <img src="../image/Two factor authentication-pana.png" alt="login">
-            </div>
-        </div>
+   <div class="container">
+      <div class="left">
+         <div class="wadah-img">
+            <img src="../image/Two factor authentication-pana.png" alt="login">
+         </div>
+      </div>
 
-        <div class="right">
-            <div class="wadah">
-                <h3>Welcome back,</h3>
-                <h2>Sign In,</h2>
+      <div class="right">
+         <div class="wadah">
+            <h3>Welcome back,</h3>
+            <h2>Sign In,</h2>
 
 
-                <form method="post">
-                    <label for="">Username</label><br>
-                    <input type="text" name="username" placeholder="yourusername" required>
-                    <br>
-                    <br>
-                    <label for="">Password</label><br>
-                    <input type="password" name="password" placeholder="********" required>
-                    <br>
+            <form method="post">
+               <label for="">Username</label><br>
+               <div class="boxx">
+                  <input class="user" type="text" name="username" placeholder="Enter Username" required>
+               </div>
+               <br>
+               <label for="">Password</label><br>
+               <div class="box">
+                  <input class="pw" type="password" id="fakePassword" name="password" placeholder="Enter Password" required>
+                  <ion-icon name="eye-outline" id="toggler"></ion-icon>
+               </div>
+               <br>
 
-                    <div class="btn">
-                        <input type="submit" name="login" value="Sign In,">
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+               <div class="btn">
+                  <input type="submit" name="login" value="Sign In,">
+               </div>
+            </form>
+         </div>
+      </div>
+   </div>
 
+   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+   <script src="script.js"></script>
 </body>
 
 </html>
